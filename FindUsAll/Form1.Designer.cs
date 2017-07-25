9324a3f6e5e5ddee5a4876e6707bb2af9d85ec32
Grzegorz Zieliński <grzesiozie15@gmail.com>
2017-07-25 16:21:33 +0100

    Created the whole app, repozistory initialization

---
+++ FindUsAll/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace FindUsAll
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.p1score = new System.Windows.Forms.Label();
+            this.p2score = new System.Windows.Forms.Label();
+            this.desc0p = new System.Windows.Forms.Label();
+            this.desc1p = new System.Windows.Forms.Label();
+            this.desc0m = new System.Windows.Forms.Label();
+            this.desc1m = new System.Windows.Forms.Label();
+            this.selectedButton0 = new System.Windows.Forms.Label();
+            this.selectedButton1 = new System.Windows.Forms.Label();
+            this.selectedButton2 = new System.Windows.Forms.Label();
+            this.selectedButton3 = new System.Windows.Forms.Label();
+            this.status = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(12, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Player 1 left:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(202, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Player 2 left:";
+            // 
+            // p1score
+            // 
+            this.p1score.AutoSize = true;
+            this.p1score.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.p1score.Location = new System.Drawing.Point(113, 53);
+            this.p1score.Name = "p1score";
+            this.p1score.Size = new System.Drawing.Size(59, 31);
+            this.p1score.TabIndex = 2;
+            this.p1score.Text = "100";
+            // 
+            // p2score
+            // 
+            this.p2score.AutoSize = true;
+            this.p2score.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.p2score.Location = new System.Drawing.Point(303, 52);
+            this.p2score.Name = "p2score";
+            this.p2score.Size = new System.Drawing.Size(59, 31);
+            this.p2score.TabIndex = 3;
+            this.p2score.Text = "100";
+            // 
+            // desc0p
+            // 
+            this.desc0p.AutoSize = true;
+            this.desc0p.Location = new System.Drawing.Point(16, 133);
+            this.desc0p.Name = "desc0p";
+            this.desc0p.Size = new System.Drawing.Size(60, 13);
+            this.desc0p.TabIndex = 4;
+            this.desc0p.Text = "AddButton:";
+            this.desc0p.Click += new System.EventHandler(this.description_Click);
+            // 
+            // desc1p
+            // 
+            this.desc1p.AutoSize = true;
+            this.desc1p.Location = new System.Drawing.Point(203, 133);
+            this.desc1p.Name = "desc1p";
+            this.desc1p.Size = new System.Drawing.Size(60, 13);
+            this.desc1p.TabIndex = 5;
+            this.desc1p.Text = "AddButton:";
+            this.desc1p.Click += new System.EventHandler(this.description_Click);
+            // 
+            // desc0m
+            // 
+            this.desc0m.AutoSize = true;
+            this.desc0m.Location = new System.Drawing.Point(16, 159);
+            this.desc0m.Name = "desc0m";
+            this.desc0m.Size = new System.Drawing.Size(63, 13);
+            this.desc0m.TabIndex = 6;
+            this.desc0m.Text = "SubtButton:";
+            this.desc0m.Click += new System.EventHandler(this.description_Click);
+            // 
+            // desc1m
+            // 
+            this.desc1m.AutoSize = true;
+            this.desc1m.Location = new System.Drawing.Point(203, 159);
+            this.desc1m.Name = "desc1m";
+            this.desc1m.Size = new System.Drawing.Size(63, 13);
+            this.desc1m.TabIndex = 7;
+            this.desc1m.Text = "SubtButton:";
+            this.desc1m.Click += new System.EventHandler(this.description_Click);
+            // 
+            // selectedButton0
+            // 
+            this.selectedButton0.AutoSize = true;
+            this.selectedButton0.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.selectedButton0.ForeColor = System.Drawing.Color.Crimson;
+            this.selectedButton0.Location = new System.Drawing.Point(82, 129);
+            this.selectedButton0.Name = "selectedButton0";
+            this.selectedButton0.Size = new System.Drawing.Size(0, 17);
+            this.selectedButton0.TabIndex = 8;
+            // 
+            // selectedButton1
+            // 
+            this.selectedButton1.AutoSize = true;
+            this.selectedButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.selectedButton1.ForeColor = System.Drawing.Color.Crimson;
+            this.selectedButton1.Location = new System.Drawing.Point(82, 157);
+            this.selectedButton1.Name = "selectedButton1";
+            this.selectedButton1.Size = new System.Drawing.Size(0, 17);
+            this.selectedButton1.TabIndex = 9;
+            // 
+            // selectedButton2
+            // 
+            this.selectedButton2.AutoSize = true;
+            this.selectedButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.selectedButton2.ForeColor = System.Drawing.Color.Crimson;
+            this.selectedButton2.Location = new System.Drawing.Point(297, 131);
+            this.selectedButton2.Name = "selectedButton2";
+            this.selectedButton2.Size = new System.Drawing.Size(0, 17);
+            this.selectedButton2.TabIndex = 10;
+            // 
+            // selectedButton3
+            // 
+            this.selectedButton3.AutoSize = true;
+            this.selectedButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.selectedButton3.ForeColor = System.Drawing.Color.Crimson;
+            this.selectedButton3.Location = new System.Drawing.Point(297, 157);
+            this.selectedButton3.Name = "selectedButton3";
+            this.selectedButton3.Size = new System.Drawing.Size(0, 17);
+            this.selectedButton3.TabIndex = 11;
+            // 
+            // status
+            // 
+            this.status.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.status.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.status.Location = new System.Drawing.Point(-8, 234);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(410, 30);
+            this.status.TabIndex = 12;
+            this.status.Text = "STATUS";
+            this.status.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(394, 333);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.selectedButton3);
+            this.Controls.Add(this.selectedButton2);
+            this.Controls.Add(this.selectedButton1);
+            this.Controls.Add(this.selectedButton0);
+            this.Controls.Add(this.desc1m);
+            this.Controls.Add(this.desc0m);
+            this.Controls.Add(this.desc1p);
+            this.Controls.Add(this.desc0p);
+            this.Controls.Add(this.p2score);
+            this.Controls.Add(this.p1score);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximumSize = new System.Drawing.Size(410, 372);
+            this.MinimumSize = new System.Drawing.Size(410, 372);
+            this.Name = "Form1";
+            this.Text = "Find Us All";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.EventKeyPress);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label p1score;
+        private System.Windows.Forms.Label p2score;
+        private System.Windows.Forms.Label desc0p;
+        private System.Windows.Forms.Label desc1p;
+        private System.Windows.Forms.Label desc0m;
+        private System.Windows.Forms.Label desc1m;
+        private System.Windows.Forms.Label selectedButton0;
+        private System.Windows.Forms.Label selectedButton1;
+        private System.Windows.Forms.Label selectedButton2;
+        private System.Windows.Forms.Label selectedButton3;
+        private System.Windows.Forms.Label status;
+    }
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # FightToWin
-bitwa o przyciski
+Prepare to the great battle of your keyboard
+
+Run the app, set keys corresponding to actions and try to be faster to reach zero presses left than your friend
+
+*warning: we are not responsible for any broken keyboard, monitors or bones
+
+--------
+solution made with Visual Studio 2017 Preview
